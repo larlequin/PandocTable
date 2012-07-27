8fc644b479bc446c0b07af722702b9bb07c84e25
GT Vallet <gtvallet@gmail.com>
2012-07-27 09:43:16 -0400

    Now able to extend existing tables

--- Table_pandoc.py
+++ Table_pandoc.py
@@ -21,41 +21,45 @@ class PandocTableCommand(sublime_plugin.TextCommand):
         # Grab the content of the current selection and split it by lines
         sel = self.view.sel()[0]
         raw_txt = self.view.substr(sel)
-        txt = raw_txt.split('\n')
-
-        # Find the number of lines and columns
-        nb_lines = len(txt)
-        nb_col = self.nber_columns(txt)
-
+        # Format raw_txt into useable data
+        txt = self.format_txt(raw_txt)
+        # Find the number of columns
+        nb_col = len(txt[0])
         # Length max by column
-        col_widths = self.len_max_col(nb_col, txt)
-
+        col_widths = self.width_col(nb_col, txt)
         # Build the final table and replace the current selection
         table = self.final_table(txt, col_widths)
         self.view.replace(edit, sel, table)
 
 
-    def nber_columns(self, txt):
-        """ Find what is the maximum number of columns
-        """
-        col_max = 0
+    def format_txt(self, txt):
+        # Remove previous table structure markers (---, ===, +, |)
+        txt = re.sub("-{3,}|={3,}|\+|\|", "", txt)
+        # Split text by lines
+        txt = txt.split('\n')
+        # Split lines by cells and create a list of lists of cells
+        new_txt = []
         for line in txt:
-            cur_col = re.subn('(\S+)(\s{2,})', '', line)[1]
-            if cur_col > col_max:
-                col_max = cur_col
-        return col_max+1
+            if line != "":
+                line = re.sub("\s+$", "", line)
+                line_split = re.compile('\t|\s{2,}').split(line)
+                new_line = []
+                for cell in line_split:
+                    new_line.append(re.sub("^\s+", "", cell))
+                new_txt.append(new_line)
+        return new_txt
 
 
-    def len_max_col(self, nb_col, txt):
+    def width_col(self, nb_col, txt):
         """ Determine the maximum possible width for each column
+            Add 2 to these widths to handle space between cell content
         """
         col_len = []
         for col in range(nb_col):
             cur_col_len = 0
             for line in txt:
-                line_split = re.compile('\s{2,}').split(line)
-                if len(line_split[col]) > cur_col_len:
-                    cur_col_len = len(line_split[col])
+                if len(line[col]) > cur_col_len:
+                    cur_col_len = len(line[col])
             col_len.append(cur_col_len)
         return map(lambda x: x + 2, col_len)
 
@@ -83,8 +87,7 @@ class PandocTableCommand(sublime_plugin.TextCommand):
             Empty cells --NA-- are replace by empty spaces
         """
         to_print = []
-        line_split = re.compile('\s{2,}').split(line)
-        for idx, item in enumerate(line_split):
+        for idx, item in enumerate(line):
             if item == "NA":
                 item = ""
             space = widths[idx]-len(item)-1
